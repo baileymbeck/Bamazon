364e916804c9058ab2988700242ced29e5860954
Bailey Beck <baileymbeck@gmail.com>
2019-10-09 16:32:47 -0500

    added mock data to table

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -12,4 +12,36 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("apple", "grocer", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("banana", "grocer", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("orange", "grocer", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pinapple", "grocer", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bread", "grocer", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("paper", "office_supply", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pens", "office_supply", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pencil", "office_supply", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ruler", "office_supply", 100, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("calender", "office_supply", 100, 5000);
+
+
+
 SELECT * FROM products;
